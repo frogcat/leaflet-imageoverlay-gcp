ae94a33dce8e1dbbd9bae437cf523c6aa52d1e34
frogcat <frogcat@users.noreply.github.com>
2019-12-20 17:06:24 +0900

    Refactor

--- leaflet-imageoverlay-gcp.js
+++ leaflet-imageoverlay-gcp.js
@@ -6,49 +6,68 @@
       interactive: true
     },
     initialize: function(url, groundControlPoints, options) {
-      this.setUrl(url);
-      this.setGroundControlPoints(groundControlPoints);
+      this._url = url;
+      this._groundControlPoints = groundControlPoints;
       L.Util.setOptions(this, options);
     },
     setOpacity: function(opacity) {
+      this.options.opacity = opacity;
       if (this._canvas) {
-        this.options.opacity = Math.min(1.0, Math.max(0.0, opacity));
-        L.DomUtil.setOpacity(this._canvas, this.options.opacity);
+        this._updateOpacity();
       }
     },
+    _updateOpacity: function() {
+      L.DomUtil.setOpacity(this._canvas, this.options.opacity);
+    },
     setGroundControlPoints: function(groundControlPoints) {
-      this.groundControlPoints = groundControlPoints;
-      this.paint();
+      this._groundControlPoints = groundControlPoints;
+      if (this._canvas) {
+        this._reset();
+      }
     },
     setUrl: function(url) {
-      var that = this;
-      delete that._image;
-      var image = new Image();
-      image.crossOrigin = "anonymous";
-      image.onload = function() {
-        that._image = image;
-        that._resize();
-      };
-      image.src = url;
+      this._url = url;
+      if (this._canvas) {
+        this._initImage();
+      }
+    },
+    _initImage: function() {
+      if (this._url.tagName === 'IMG') {
+        this._rawImage = this._url;
+        this._url = this._rawImage.src;
+        this._reset();
+      } else {
+        var that = this;
+        delete that._image;
+        var image = new Image();
+        image.crossOrigin = "anonymous";
+        image.onload = function() {
+          that._rawImage = image;
+          that._reset();
+          that.fire('load');
+        };
+        image.src = this._url;
+      }
+
     },
     getEvents: function() {
       return {
-        zoom: this.paint,
-        viewreset: this.paint,
-        moveend: this.paint,
-        resize: this._resize
+        zoom: this._reset,
+        viewreset: this._reset,
+        moveend: this._reset,
+        resize: this._reset
       };
     },
     onAdd: function(map) {
       this._canvas = document.createElement("canvas");
-      this.setOpacity(this.options.opacity);
       this._context = this._canvas.getContext("morph");
+      this._updateOpacity();
       this.getPane().appendChild(this._canvas);
       if (this.options.interactive) {
         L.DomUtil.addClass(this._canvas, 'leaflet-interactive');
         this.addInteractiveTarget(this._canvas);
       }
-      this._resize();
+      this._initImage();
     },
     onRemove: function(map) {
       L.DomUtil.remove(this._canvas);
@@ -56,33 +75,25 @@
         this.removeInteractiveTarget(this._canvas);
       }
     },
-    _resize: function() {
-      if (this._map && this._canvas) {
-        var s = this._map.getSize();
-        this._canvas.width = s.x;
-        this._canvas.height = s.y;
-        this._canvas.style.width = s.x + "px";
-        this._canvas.style.height = s.y + "px";
-        this.paint();
-      }
-    },
-    paint: function() {
-      if (!this._map || !this._canvas || !this._context || !this._image) return;
-      var canvas = this._canvas;
-      var context = this._context;
-      var image = this._image;
-      var map = this._map;
-      var groundControlPoints = this.groundControlPoints;
-      L.DomUtil.setPosition(canvas, map.containerPointToLayerPoint([0, 0]));
-      context.clear();
-      context.drawImage(image, groundControlPoints.map(function(a) {
+    _reset: function() {
+      if (!this._canvas || !this._rawImage) return;
+
+      var s = this._map.getSize();
+      this._canvas.width = s.x;
+      this._canvas.height = s.y;
+      this._canvas.style.width = s.x + "px";
+      this._canvas.style.height = s.y + "px";
+      L.DomUtil.setPosition(this._canvas, this._map.containerPointToLayerPoint([0, 0]));
+
+      this._context.clear();
+      this._context.drawImage(this._rawImage, this._groundControlPoints.map(function(a) {
         var p = a.imagePoint;
         var q = map.latLngToContainerPoint(a.latlng);
         return [p.x, p.y, q.x, q.y];
       }));
     },
     containerPointToImagePoint: function(containerPoint) {
-      if (this._context) {
+      if (this._canvas && this._rawImage) {
         var xy = this._context.getTexturePointAt(containerPoint.x, containerPoint.y);
         if (xy) return L.point(xy[0], xy[1]);
       }
