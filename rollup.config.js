dc2a165d7ee43306da034bfd3c65ba360fa0238a
frogcat <frogcat@users.noreply.github.com>
2020-02-07 20:03:51 +0900

    Change build dir, add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
 import buble from '@rollup/plugin-buble';
+import {
+  terser
+} from 'rollup-plugin-terser';
 
 export default {
   input: 'leaflet-imageoverlay-gcp.js',
-  output: {
-    file: 'docs/leaflet-imageoverlay-gcp.js',
+  output: [{
+    file: 'dist/leaflet-imageoverlay-gcp.js',
     format: 'iife'
-  },
+  }, {
+    file: 'dist/leaflet-imageoverlay-gcp.min.js',
+    format: 'iife',
+    plugins: [terser()]
+  }],
   plugins: [
     resolve(),
     buble()
